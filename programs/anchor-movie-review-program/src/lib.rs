fd1f4146999acd0a7f059f265bb438510d25eec9
dwuw <shichen1iu@outlook.com>
2024-09-17 20:07:43 +0800

    增加代币奖励/更新功能

--- programs/anchor-movie-review-program/src/lib.rs
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -1,28 +1,54 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
 
 declare_id!("5BhgkamwCK6zk8e8W8SMc1hZDbXukvpSoVQwHifLFDkz");
 
 #[program]
 pub mod anchor_movie_review_program {
+    use anchor_spl::token::{mint_to, MintTo};
+
     use super::*;
 
-    //1.添加电影评论指令
+    //1.添加电影评论并发送代币作为奖励 指令
     pub fn add_movie_review(
         ctx: Context<AddMovieReview>,
         title: String,
         description: String,
         rating: u8,
     ) -> Result<()> {
-        msg!("Movie Review Account Created");
+        msg!("Movie review account created");
         msg!("Title: {}", title);
         msg!("Description: {}", description);
         msg!("Rating: {}", rating);
 
+        require!(rating >= 1 && rating <= 5, MovieReviewError::InvalidRating);
+
         let movie_review = &mut ctx.accounts.movie_review;
         movie_review.reviewer = ctx.accounts.initializer.key();
         movie_review.title = title;
-        movie_review.rating = rating;
         movie_review.description = description;
+        movie_review.rating = rating;
+
+        let bump = ctx.bumps.mint; // 直接访问 bump 字段
+
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.token_account.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+                &[&["mint".as_bytes(), &[bump]]],
+            ),
+            10 * 10 ^ 6,
+        )?;
+
+        msg!("Minted tokens");
+
         Ok(())
     }
 
@@ -38,9 +64,11 @@ pub mod anchor_movie_review_program {
         msg!("Description: {}", description);
         msg!("Rating: {}", rating);
 
+        require!(rating >= 1 && rating <= 5, MovieReviewError::InvalidRating);
+
         let movie_review = &mut ctx.accounts.movie_review;
-        movie_review.rating = rating;
         movie_review.description = description;
+        movie_review.rating = rating;
 
         Ok(())
     }
@@ -50,15 +78,21 @@ pub mod anchor_movie_review_program {
         msg!("Movie review for {} deleted", title);
         Ok(())
     }
+
+    //4.初始化平陵奖励代币
+    pub fn initialize_token_mint(_ctx: Context<InitializeMint>) -> Result<()> {
+        msg!("Token mint initialized");
+        Ok(())
+    }
 }
 
 //1.添加电影评论指令所需账户
 #[derive(Accounts)]
-#[instruction(title:String, description:String)]
+#[instruction(title: String, description: String)]
 pub struct AddMovieReview<'info> {
     #[account(
         init,
-        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        seeds=[title.as_bytes(), initializer.key().as_ref()],
         bump,
         payer = initializer,
         space = 8 + 32 + 1 + 4 + title.len() + 4 + description.len()
@@ -67,6 +101,25 @@ pub struct AddMovieReview<'info> {
     #[account(mut)]
     pub initializer: Signer<'info>,
     pub system_program: Program<'info, System>,
+    // ADDED ACCOUNTS BELOW
+    pub token_program: Program<'info, Token>,
+    // 初始化铸币厂Mint Account
+    #[account(
+        seeds = ["mint".as_bytes()],
+        bump,
+        mut
+    )]
+    pub mint: Account<'info, Mint>,
+    //初始化代币账户
+    #[account(
+        init_if_needed,
+        payer = initializer,
+        associated_token::mint = mint,
+        associated_token::authority = initializer
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 //2.更新电影评论指令所需账户
@@ -103,6 +156,25 @@ pub struct DeleteMovieReview<'info> {
     pub system_program: Program<'info, System>,
 }
 
+//4.初始化平陵奖励代币所需账户
+#[derive(Accounts)]
+pub struct InitializeMint<'info> {
+    #[account(
+        init,
+        seeds = ["mint".as_bytes()],
+        bump,
+        payer = user,
+        mint::decimals = 6,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
+
 //0.电影评论的结构体
 #[account]
 pub struct MovieAccountState {
@@ -110,4 +182,11 @@ pub struct MovieAccountState {
     pub rating: u8,          // 1
     pub title: String,       // 4 + len()
     pub description: String, // 4 + len()
+}
+
+//自定义错误
+#[error_code]
+enum MovieReviewError {
+    #[msg("Rating must be between 1 and 5")]
+    InvalidRating,
 }
--- tests/anchor-movie-review-program.ts
+++ tests/anchor-movie-review-program.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { assert, expect } from "chai";
+import { getAssociatedTokenAddress, getAccount } from "@solana/spl-token";
 import { AnchorMovieReviewProgram } from "../target/types/anchor_movie_review_program";
 
 describe("anchor-movie-review-program", () => {
@@ -22,10 +23,24 @@ describe("anchor-movie-review-program", () => {
     program.programId
   );
 
+  //获取铸币厂地址
+  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("mint")],
+    program.programId
+  );
+
   //1.添加电影评论测试
   it("Movie review is added`", async () => {
+    const tokenAccount = await getAssociatedTokenAddress(
+      mint,
+      provider.wallet.publicKey
+    );
+
     const tx = await program.methods
       .addMovieReview(movie.title, movie.description, movie.rating)
+      .accounts({
+        tokenAccount: tokenAccount,
+      })
       .rpc();
 
     const account = await program.account.movieAccountState.fetch(moviePda);
@@ -33,6 +48,9 @@ describe("anchor-movie-review-program", () => {
     expect(movie.rating === account.rating);
     expect(movie.description === account.description);
     expect(account.reviewer === provider.wallet.publicKey);
+
+    const userAta = await getAccount(provider.connection, tokenAccount);
+    expect(Number(userAta.amount)).to.equal((10 * 10) ^ 6);
   });
   //2.更新电影评论测试
   it("Movie review is updated`", async () => {
@@ -52,5 +70,10 @@ describe("anchor-movie-review-program", () => {
   //3.删除电影评论测试
   it("Deletes a movie review", async () => {
     const tx = await program.methods.deleteMovieReview(movie.title).rpc();
+  });
+
+  //4.代币奖励测试
+  it("Initializes the reward token", async () => {
+    const tx = await program.methods.initializeTokenMint().rpc();
   });
 });
