1b1e6feab99f47ac07007eaee614f06ebf4e5f9c
dwuw <shichen1iu@outlook.com>
2024-09-17 16:30:50 +0800

    first commit

---
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -0,0 +1,113 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5BhgkamwCK6zk8e8W8SMc1hZDbXukvpSoVQwHifLFDkz");
+
+#[program]
+pub mod anchor_movie_review_program {
+    use super::*;
+
+    //1.添加电影评论指令
+    pub fn add_movie_review(
+        ctx: Context<AddMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie Review Account Created");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.reviewer = ctx.accounts.initializer.key();
+        movie_review.title = title;
+        movie_review.rating = rating;
+        movie_review.description = description;
+        Ok(())
+    }
+
+    //2.更新电影评论指令
+    pub fn update_movie_review(
+        ctx: Context<UpdateMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie review account space reallocated");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.rating = rating;
+        movie_review.description = description;
+
+        Ok(())
+    }
+
+    //3.删除电影评论指令
+    pub fn delete_movie_review(_ctx: Context<DeleteMovieReview>, title: String) -> Result<()> {
+        msg!("Movie review for {} deleted", title);
+        Ok(())
+    }
+}
+
+//1.添加电影评论指令所需账户
+#[derive(Accounts)]
+#[instruction(title:String, description:String)]
+pub struct AddMovieReview<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        payer = initializer,
+        space = 8 + 32 + 1 + 4 + title.len() + 4 + description.len()
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+//2.更新电影评论指令所需账户
+#[derive(Accounts)]
+#[instruction(title:String, description:String)]
+pub struct UpdateMovieReview<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 1 + 4 + title.len() + 4 + description.len(),
+        realloc::payer = initializer,
+        realloc::zero = true,
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+//3.删除电影评论所需账户
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteMovieReview<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        close=initializer
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+//0.电影评论的结构体
+#[account]
+pub struct MovieAccountState {
+    pub reviewer: Pubkey,    // 32
+    pub rating: u8,          // 1
+    pub title: String,       // 4 + len()
+    pub description: String, // 4 + len()
+}
---
+++ tests/anchor-movie-review-program.ts
@@ -0,0 +1,56 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { assert, expect } from "chai";
+import { AnchorMovieReviewProgram } from "../target/types/anchor_movie_review_program";
+
+describe("anchor-movie-review-program", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace
+    .AnchorMovieReviewProgram as Program<AnchorMovieReviewProgram>;
+
+  const movie = {
+    title: "Valroant",
+    description: "Perfect!!!!",
+    rating: 10,
+  };
+
+  const [moviePda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(movie.title), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  );
+
+  //1.添加电影评论测试
+  it("Movie review is added`", async () => {
+    const tx = await program.methods
+      .addMovieReview(movie.title, movie.description, movie.rating)
+      .rpc();
+
+    const account = await program.account.movieAccountState.fetch(moviePda);
+    expect(movie.title === account.title);
+    expect(movie.rating === account.rating);
+    expect(movie.description === account.description);
+    expect(account.reviewer === provider.wallet.publicKey);
+  });
+  //2.更新电影评论测试
+  it("Movie review is updated`", async () => {
+    const newDescription = "Wow this is new";
+    const newRating = 4;
+
+    const tx = await program.methods
+      .updateMovieReview(movie.title, newDescription, newRating)
+      .rpc();
+
+    const account = await program.account.movieAccountState.fetch(moviePda);
+    expect(movie.title === account.title);
+    expect(newRating === account.rating);
+    expect(newDescription === account.description);
+    expect(account.reviewer === provider.wallet.publicKey);
+  });
+  //3.删除电影评论测试
+  it("Deletes a movie review", async () => {
+    const tx = await program.methods.deleteMovieReview(movie.title).rpc();
+  });
+});
